f6a6cf60960b3a88c20f4068ce35b878a1c4142a
wangxin <wangxinnn3@gmail.com>
2026-09-05 14:12:31 +0800

    chore(sdk): consume shared agent domain

---
+++ sdk/gateway/codepet-agent-sdk/lib/codepet_agent_sdk.dart
@@ -0,0 +1,5 @@
+/// Generated, shared CodePet Agent domain protocol types and codecs.
+library;
+
+export 'package:codepet_core_sdk/codepet_core_sdk.dart';
+export 'src/generated.dart';
--- sdk/gateway/codepet-gateway-sdk/lib/codepet_gateway_sdk.dart
+++ sdk/gateway/codepet-gateway-sdk/lib/codepet_gateway_sdk.dart
@@ -2,4 +2,5 @@
 library;
 
 export 'package:codepet_core_sdk/codepet_core_sdk.dart';
+export 'package:codepet_agent_sdk/codepet_agent_sdk.dart';
 export 'src/generated.dart';
